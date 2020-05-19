68c1ce961d04c606d80614e6d6fb597e75b71bae
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-19 21:50:18 +0530

    Adds TypeAhead Controller

---
+++ Assignment.API/Controllers/TypeAheadController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Assignment.Business;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.API
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class AssignmentController: ControllerBase
+    {
+        private readonly ILocationService _service;
+        public AssignmentController(ILocationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("locations")]
+        public async Task<IActionResult> GetTypeHeadHandler()
+        {
+            int partnerId = Convert.ToInt32((string)HttpContext.Request.Query["partnerid"]);
+            string searchQuery = (string)HttpContext.Request.Query["searchTerm"];
+            string types = (string)HttpContext.Request.Query["types"];
+            bool includeAll = Convert.ToBoolean((string)HttpContext.Request.Query["types"]);
+
+            return Ok(await _service.GetAllLocationsAsync(partnerId, searchQuery, types, includeAll));
+        }
+    }
+}
