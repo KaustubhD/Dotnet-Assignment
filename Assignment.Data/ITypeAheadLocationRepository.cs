57d8354eb123e187808b70a4473242e15e135ccc
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-19 20:13:50 +0530

    Adds namespace in Repos

--- Assignment.Data/ITypeAheadLocationRepository.cs
+++ Assignment.Data/ITypeAheadLocationRepository.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Assignment.Core;
 
-public interface ITypeAheadLocationRepository
+namespace Assignment.Data
 {
-    Task<ICollection<Location>> GetAllLocationsAsync(int partnerId, string search, string Types, bool IncludeAll);
+    public interface ITypeAheadLocationRepository
+    {
+        Task<ICollection<Location>> GetAllLocationsAsync(int partnerId, string search, string Types, bool IncludeAll);
+    }
 }
--- Assignment.Data/Implementation/TypeAheadLocationRepository.cs
+++ Assignment.Data/Implementation/TypeAheadLocationRepository.cs
@@ -4,22 +4,25 @@ using System.Threading.Tasks;
 using Assignment.Core;
 using RestSharp;
 
-public class TypeAheadLocationRepository: ITypeAheadLocationRepository
+namespace Assignment.Data
 {
-    public TypeAheadLocationRepository()
+    public class TypeAheadLocationRepository: ITypeAheadLocationRepository
     {
+        public TypeAheadLocationRepository()
+        {
 
-    }
+        }
 
-    public async Task<ICollection<Location>> GetAllLocationsAsync(int PartnerId, string SearchQuery, string Types = null, bool IncludeAll = false)
-    {
-        var client = new RestClient("https://https://sprint-api.newhomesource.com/api/v2");
+        public async Task<ICollection<Location>> GetAllLocationsAsync(int PartnerId, string SearchQuery, string Types = null, bool IncludeAll = false)
+        {
+            var client = new RestClient("https://https://sprint-api.newhomesource.com/api/v2");
 
-        var request = new RestRequest("Typeahead/Locations", DataFormat.Json);
+            var request = new RestRequest("Typeahead/Locations", DataFormat.Json);
 
-        request.AddParameter("partnerid", PartnerId);
-        request.AddParameter("search", SearchQuery);
+            request.AddParameter("partnerid", PartnerId);
+            request.AddParameter("search", SearchQuery);
 
-        return await client.GetAsync<List<Location>>(request);
+            return await client.GetAsync<List<Location>>(request);
+        }
     }
 }
