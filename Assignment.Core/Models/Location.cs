969ade42e941c0262442806cd6b938c550dc2559
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-19 21:50:38 +0530

    Adds getter setters on Model

--- Assignment.Core/Models/Location.cs
+++ Assignment.Core/Models/Location.cs
@@ -4,19 +4,19 @@ namespace Assignment.Core
 {
     public class Location
     {
-        public int Id;
-        public string Name;
-        public string Type;
-        public int MarketId;
-        public string MarketName;
-        public string MarketStateAbbr;
-        public string MarketStateName;
-        public double Latitude;
-        public double Longitude;
-        public string City;
-        public string State;
-        public bool HasShowCase;
-        public string BrandType;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int MarketId { get; set; }
+        public string MarketName { get; set; }
+        public string MarketStateAbbr { get; set; }
+        public string MarketStateName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public bool HasShowCase { get; set; }
+        public string BrandType { get; set; }
         
         public Location()
         {
