7866e8f6424f62cad31f10f6e83af1c83d13621e
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-19 20:13:09 +0530

    Syncs DI package and adds DI class

---
+++ Assignment.Root/DI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Assignment.Data;
+using Assignment.Business;
+
+namespace Assignment.Root
+{
+    public class DI
+    {
+        public DI()
+        {
+
+        }
+        public static void AddConfig(IServiceCollection services)
+        {
+            services.AddScoped<ITypeAheadLocationRepository, TypeAheadLocationRepository>();
+            services.AddScoped<ILocationService, LocationService>();
+        }
+    }
+}
