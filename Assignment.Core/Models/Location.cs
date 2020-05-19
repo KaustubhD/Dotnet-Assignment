9ab58db52340a56e9df1ec26bbb055309e9f51f9
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-19 15:09:06 +0530

    Adds Location model

---
+++ Assignment.Core/Models/Location.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Assignment.Core
+{
+    public class Location
+    {
+        public int Id;
+        public string Name;
+        public string Type;
+        public int MarketId;
+        public string MarketName;
+        public string MarketStateAbbr;
+        public string MarketStateName;
+        public double Latitude;
+        public double Longitude;
+        public string City;
+        public string State;
+        public bool HasShowCase;
+        public string BrandType;
+        
+        public Location()
+        {
+
+        }
+    }
+}
