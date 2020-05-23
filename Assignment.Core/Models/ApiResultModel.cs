b1a21e987de53af08ed8f1763fb006f4286058b5
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-23 15:41:13 +0530

    Renames result model and made it generic

--- Assignment.Core/Models/ApiResultModel.cs
+++ Assignment.Core/Models/ApiResultModel.cs
@@ -1,12 +1,12 @@
 namespace Assignment.Core
 {
-    public class ApiResultModelOfBoolean
+    public class ApiResultModel<T>
     {
         public string Time { get; set; }
         public string ErrorMessage { get; set; }
         public string Server { get; set; }
         public string Status { get; set; }
-        public bool Result { get; set; }
+        public T Result { get; set; }
 
     }
 }
