6c5cabd06dd8741882a0fb54b440f478c2a56329
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-05-23 21:25:49 +0530

    API 6: Updates bookmark model to work with create bookmark

--- Assignment.Core/Models/BookmarkActionType.cs
+++ Assignment.Core/Models/BookmarkActionType.cs
@@ -1,13 +1,22 @@
+using System.Runtime.Serialization;
+
 namespace Assignment.Core
 {
     public enum BookmarkActionType
     {
+        [EnumMember(Value = "Saved")]
         Saved = 1,
+        [EnumMember(Value = "Like")]
         Like = 2,
+        [EnumMember(Value = "Dislike")]
         Dislike = 3,
+        [EnumMember(Value = "Board")]
         Board = 4,
+        [EnumMember(Value = "Lead")]
         Lead = 5,
+        [EnumMember(Value = "Reco")]
         Reco = 6,
+        [EnumMember(Value = "Neutral")]
         Neutral = 7
     }
 }
--- Assignment.Core/Models/BookmarkAttribute.cs
+++ Assignment.Core/Models/BookmarkAttribute.cs
@@ -1,21 +1,43 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Assignment.Core
 {
+    [AtLeastOne("ContentTypeName", "ContentTypeId")]
+    #nullable enable
     public class BookmarkAttribute
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
+        [Required]
         public string UserId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Initials { get; set; }
-        public int ContentTypeId { get; set; }
-        public string ContentTypeName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Initials { get; set; }
+        public int? ContentTypeId { get; set; }
+
+        /*
+            Both ContentTypeId i.e 👆🏻 and
+            ContentTypeName i.e 👇 are required but they are taken care of
+            by the attribute on top
+        */
+        public string? ContentTypeName { get; set; }
+        // [Required]
         public int ContentId { get; set; }
+        // [Required]
         public string Title { get; set; }
+        // [Required]
         public string Url { get; set; }
+        [Required]
+        [DefaultValue("Saved")]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+
         public BookmarkActionType Action { get; set; }
-        public List<UserProfile> SharedUsers { get; set; }
-        public List<BookmarkData> BookmarkData { get; set; }
+        internal List<UserProfile>? SharedUsers { get; set; }
+        internal List<BookmarkData>? BookmarkData { get; set; }
+
+        // only needed for creating or editing bookmarks
+        public string? Notes { get; set; }
     }
 }
