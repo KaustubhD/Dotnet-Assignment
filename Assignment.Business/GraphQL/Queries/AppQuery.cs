29edcf407ab2d59b653f83835604a9010298b463
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-07-03 19:50:53 +0530

    API request with a single query parameter

--- Assignment.Business/GraphQL/Queries/AppQuery.cs
+++ Assignment.Business/GraphQL/Queries/AppQuery.cs
@@ -2,6 +2,7 @@
 using Assignment.Business.GraphQL.Types;
 using Assignment.Data;
 using GraphQL.Types;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Assignment.BUsiness.GraphQL.Queries
@@ -12,7 +13,10 @@ namespace Assignment.BUsiness.GraphQL.Queries
         {
             Field<ListGraphType<LocationType>>(
                "location",
-               resolve: context => repository.GetAllLocations()
+               arguments: new QueryArguments(
+                   new QueryArgument<StringGraphType> { Name = "searchTerm" }
+               ),
+               resolve: context => repository.GetAllLocations(new Core.LocationDto() { SearchTerm = context.GetArgument<string>("searchTerm") })
             );
         }
     }
